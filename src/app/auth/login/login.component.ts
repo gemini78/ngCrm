7733ed7606d9358581f9a5c8801eaea4f917cc0e
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-08 18:08:05 +0200

    First Commit

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `
+  <div class="bg-light rounded p-3">
+      <h1>Connexion à NgCRM !</h1>
+      <form>
+        <div>
+          <label class="mb-1" for="email">Adresse email</label>
+          <input
+            type="email"
+            placeholder="Adresse email de connexion"
+            name="email"
+            id="email"
+            class="mb-3 form-control"
+          />
+          <p class="invalid-feedback">L'adresse email doit être valide</p>
+        </div>
+        <div>
+          <label class="mb-1" for="password">Mot de passe</label>
+          <input
+            type="password"
+            placeholder="Votre mot de passe"
+            name="password"
+            id="password"
+            class="mb-3 form-control"
+          />
+          <p class="invalid-feedback">
+            Le mot de passe est obligatoire, doit faire 8 caractères minimum et
+            contenir au moins un chiffre
+          </p>
+        </div>
+
+        <button class="btn btn-success">Connexion !</button>
+      </form>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
