cb2e2ff210532e2c4161361c50247786c7d22bd5
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-09 23:35:01 +0200

    The authStatus$ Observable

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { BehaviorSubject, map, tap } from "rxjs";
 
 export type TRegisterData = {
     email: string;
     name: string;
     password: string;
-}
+};
 
 export type TLoginData = {
     email: string;
     password: string;
-}
+};
+
+export type TLoginApiResponse = {
+    authToken: string
+};
 
 @Injectable()
 export class AuthService {
+    authStatus$ = new BehaviorSubject(false);
+    authToken: string | null = null;
     constructor(private http: HttpClient) { }
 
     register(registerData: TRegisterData) {
@@ -27,7 +33,18 @@ export class AuthService {
     }
 
     login(loginData: TLoginData) {
-        return this.http.post('https://x8ki-letl-twmt.n7.xano.io/api:BTcrjDR0/auth/login', loginData)
+        return this.http.post<TLoginApiResponse>('https://x8ki-letl-twmt.n7.xano.io/api:BTcrjDR0/auth/login', loginData).pipe(
+            map(apiResponse => apiResponse.authToken),
+            tap(token => {
+                this.authToken = token;
+                this.authStatus$.next(true);
+            })
+        )
+    }
+
+    logout() {
+        this.authToken = null;
+        this.authStatus$.next(false);
     }
 
 }
