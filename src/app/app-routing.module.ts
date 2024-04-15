e636c2ff8da99bdf02c415d4783751a702f2259a
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-15 16:50:07 +0200

    Setting up the Invoice module and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: 'account', loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule) },
+  { path: 'invoices', loadChildren: () => import('./invoice/invoice.module').then(module => module.InvoiceModule) },
 ];
 
 @NgModule({
