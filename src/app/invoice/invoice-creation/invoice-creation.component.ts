88b800d5cc429d2044e8f8532bea5ca74df465a6
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-17 23:58:24 +0200

    Redirection after submission

--- src/app/invoice/invoice-creation/invoice-creation.component.ts
+++ src/app/invoice/invoice-creation/invoice-creation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TInvoice } from '../invoice';
 import { InvoiceService } from '../invoice.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-creation',
@@ -11,6 +12,9 @@ import { InvoiceService } from '../invoice.service';
     Remplissez les informations de la facture afin de la retrouver dans
     votre liste plus tard !
   </p>
+  <p class="alert bg-warning text-white" *ngIf="errorMessage">
+    {{errorMessage}}
+  </p>
 
   <app-invoice-form (invoice-form)="onSubmit($event)" ></app-invoice-form>
 
@@ -20,17 +24,19 @@ import { InvoiceService } from '../invoice.service';
   ]
 })
 export class InvoiceCreationComponent implements OnInit {
+  errorMessage = '';
 
-  constructor(private service: InvoiceService) { }
+  constructor(private service: InvoiceService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(invoiceData: TInvoice) {
     this.service.create(invoiceData).subscribe({
-      next: () => console.log(),
-      error: (error) => console.log(error)
+      next: () => this.router.navigate(['../'], {
+        relativeTo: this.route
+      }),
+      error: (error) => (this.errorMessage = 'Une erreur est survenue, merci de réessayer plus tard'),
     })
-    console.log(invoiceData);
   }
 }
