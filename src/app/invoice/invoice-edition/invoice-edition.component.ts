d7026dd65c2edfad1e17b923c49d607d2c1dbc57
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-21 01:10:21 +0200

    Submission management and redirection

--- src/app/invoice/invoice-edition/invoice-edition.component.ts
+++ src/app/invoice/invoice-edition/invoice-edition.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TInvoice } from '../invoice';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { InvoiceService } from '../invoice.service';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable, map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-invoice-edition',
@@ -27,14 +27,27 @@ import { Observable, map, switchMap } from 'rxjs';
 export class InvoiceEditionComponent implements OnInit {
   errorMessage = '';
   invoice$?: Observable<TInvoice>;
+  invoiceId?: number;
 
-  constructor(private route: ActivatedRoute, private service: InvoiceService) { }
+  constructor(private route: ActivatedRoute, private service: InvoiceService, private router: Router) { }
 
   ngOnInit(): void {
     this.invoice$ = this.route.paramMap.pipe(
       map(paramMap => paramMap.get('id')),
+      tap(id => this.invoiceId = +id!),
       switchMap(id => this.service.find(+id!))
     )
   }
-  onSubmit(invoice: TInvoice) { }
+  onSubmit(invoice: TInvoice) {
+    this.service.update({ ...invoice, id: this.invoiceId }).subscribe({
+      next: () => {
+        this.router.navigate(['../'], {
+          relativeTo: this.route
+        })
+      },
+      error: () => {
+        this.errorMessage = "Une erreur est survenue, merci de réessayer plus tard, ou de contacter le service client"
+      }
+    });
+  }
 }
